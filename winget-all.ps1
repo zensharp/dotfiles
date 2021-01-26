873e7da6812d497fd4cda2fd1f45649b1ee6233f
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-01-26 11:15:25 -0800

    Add winget script

---
+++ winget-all.ps1
@@ -0,0 +1,32 @@
+# Make sure 1) winget is installed and 2) you are running Powershell as admin
+
+# OS
+winget install "Windows Terminal"
+winget install "7zip"
+winget install "Malwarebytes"
+winget install "Git"
+winget install "Python"
+winget install "Ubuntu"
+
+# Hardware
+winget install "Nvidia GeForce Experience"
+winget install "iCUE"
+winget install "Logitech Options"
+
+# Dev
+winget install "Visual Studio Code"
+winget install "Visual Studio Community" --override "--norestart"
+winget install "Sourcetree"
+
+# Apps
+winget install "Mozilla Firefox"
+winget install "1Password"
+winget install "iTunes"
+winget install "Spotify"
+winget install "Slack"
+winget install "Discord"
+winget install "Unity Hub"
+
+# Extra
+#winget install "OBS Studio"
+#winget install "TeXstudio"
