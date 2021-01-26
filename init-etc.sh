010cc55ad2ce7c5d20e6d26ad41ec4f170918f91
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-01-26 10:24:33 -0800

    Add bash settings initialization

--- init-etc.sh
+++ init-etc.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
-usernameUbuntu="developer"
-usernameWindows="dev"
+if [ $# -ge 2 ]
+then
+    usernameLinux=$1
+    usernameHost=$2
+else
+    read -p "Linux username: " usernameLinux
+    read -p "Host username: " usernameHost
+fi
 
-echo "s/\/home\/$usernameUbuntu/\/mnt\/c\/Users\/$usernameWindows/g"
-sed -i "s/\/home\/$usernameUbuntu/\/mnt\/c\/Users\/$usernameWindows/g" /etc/passwd
+echo
 
+# Change home to Windows user directory
+homeLinux="/home/$usernameLinux"
+home="/mnt/c/Users/$usernameHost"
+echo "Changing path from \"$homeLinux\" to \"$home\"..."
+regex="s/\/home\/$usernameLinux/\/mnt\/c\/Users\/$usernameHost/g"
+sed -i "$regex" /etc/passwd
+
+# Apply Windows-friendly WSL settings
+echo "Applying WSL settings..."
 cp wsl.conf /etc
+
+# Add standard files to new home
+echo "Adding standard files to home..."
+cp -R /etc/skel/. "$home/"
+
+# Add common aliases
+echo
+echo "Adding windows aliases..."
+echo "alias explorer=explorer.exe" >> "$home"/.bashrc
+echo "alias clip=clip.exe" >> "$home"/.bashrc
+echo "alias gitk=gitk.exe" >> "$home"/.bashrc
