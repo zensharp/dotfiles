d90b85e8a2e338b5f479998d6ac1c48c3f24016b
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-01-27 00:51:54 -0800

    Protect users against unintentional configuration

--- init-user.sh
+++ init-user.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+if [ $(whoami) = "root" ]
+then
+	echo "Run this as the user you want to configure." >&2 
+	echo "Example: sudo -u johndoe"
+	exit 1
+fi
+
 # Apply Windows-friendly WSL settings
 echo "Applying WSL settings..."
 cp etc/wsl.conf /etc
 
 # Add standard files to new home
 echo "Adding standard files to home..."
-cp -R /etc/skel/. "$home/"
+cp -R /etc/skel/. "$HOME/"
 
 # Add common aliases
 echo
 echo "Adding windows aliases..."
-echo "alias explorer=explorer.exe" >> "$home"/.bashrc
-echo "alias clip=clip.exe" >> "$home"/.bashrc
-echo "alias gitk=gitk.exe" >> "$home"/.bashrc
+echo "alias explorer=explorer.exe" >> "$HOME"/.bashrc
+echo "alias clip=clip.exe" >> "$HOME"/.bashrc
+echo "alias gitk=gitk.exe" >> "$HOME"/.bashrc
