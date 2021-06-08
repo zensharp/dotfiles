095f62887dd91ead9d2cb630ef6e5c01a961dcd6
Andrew <andrewmichaeljordan@outlook.com>
2021-06-08 16:18:00 -0700

    update

--- windows/winget-all.ps1
+++ windows/winget-all.ps1
@@ -4,24 +4,18 @@
 winget install "7zip"
 winget install "Git"
 winget install "Git Credential Manager Core"
-winget install "Ubuntu"
 winget install "Python"
 winget install "Nodejs"
 winget install "Link Shell Extension"
 
 # Essentials
 winget install "Windows Terminal"
-winget install "Mozilla Firefox"
-winget install "Malwarebytes"
-winget install "1Password"
 
 # Hardware
 winget install "Nvidia GeForce Experience"
-winget install "iCUE"
-winget install "Logitech Options"
 
 # Dev
-winget install "vscode-user-x64" --override "/silent /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
+winget install -e --id Microsoft.VisualStudioCode
 winget install "Visual Studio Community" --override "--norestart"
 winget install "Sourcetree"
 winget install "Unity Hub"
@@ -32,12 +26,3 @@ winget install "Spotify"
 winget install "Slack"
 winget install "Discord"
 winget install "Steam"
-
-# Extra
-#winget install "Audacity"
-#winget install "OBS Studio"
-#winget install "TeXstudio"
-#winget install "Mp3tag"
-#winget install "FileConverter"
-#winget install "ShellExView"
-#winget install "Power Toys"
