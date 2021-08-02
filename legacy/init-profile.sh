3b9fe1e63ad7fb91a446d7ee4ed3ee163b58f562
Andrew <andrewmichaeljordan@outlook.com>
2021-08-02 11:17:41 -0700

    delete legacy files

--- legacy/init-profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-profile="$HOME/.bashrc"
-
-cat profile/mkcd >> $profile
-cat profile/wslcd >> $profile
-cat profile/interop >> $profile
-cat profile/homebrew >> $profile
-cat profile/autojump >> $profile
