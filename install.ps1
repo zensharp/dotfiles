011eefca1123bd72e81256c00e89157228cbe2c9
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-06 17:08:59 -0700

    add experimental windows installer

---
+++ install.ps1
@@ -0,0 +1,6 @@
+﻿$dotfiles = "$env:USERPROFILE\.dotfiles"
+
+Copy-Item -Recurse -Force $dotfiles/ueli $env:APPDATA/euli
+Copy-Item -Recurse -Force $dotfiles/windows-terminal/* $env:LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+Copy-Item -Recurse -Force $dotfiles/vscode $env:APPDATA/Code/User
+Copy-Item -Recurse -Force $dotfiles/atom $env:USERPROFILE/.atom
