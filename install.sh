b8c215dfdaf5e8075886e3c0d5cd59b18ee20cf5
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-07 20:16:46 -0700

    improve install script

--- install.sh
+++ install.sh
@@ -14,15 +14,16 @@ sudo apt install -y autojump
 
 # Starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
-cp ~/dotfiles/starship/starship.toml ~/.config
+cp ~/dotfiles/starship/starship.toml ~/.config/
 
 # FD
 sudo apt install -y fd-find
 ln --symbolic $(which fdfind) ~/.local/bin/fd
 
 # Micro
-curl https://getmic.ro | bash && sudo mv micro /usr/bin
-cp ~/dotfiles/micro/settings.json ~/.config/micro
+curl https://getmic.ro | bash
+sudo mv micro /usr/bin
+cp ~/dotfiles/micro/settings.json ~/.config/micro/
 
 # Sift
 curl https://sift-tool.org/downloads/sift/sift_latest_linux_amd64.tar.gz | tar -xz
@@ -42,7 +43,7 @@ ln -s $(which batcat) ~/.local/bin/bat
 
 # Zshell
 sudo apt install -y zsh
-chsh --shell $(which zsh)
+sudo chsh --shell $(which zsh) $(whoami)
 
 # Python
 sudo apt install -y python3-pip
