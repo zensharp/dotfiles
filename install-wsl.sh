421be01c6c44eb97391c7f8f08aacadf6e2c3fdc
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-08 10:53:18 -0700

    apply more sane naming

--- install-wsl.sh
+++ install-wsl.sh
@@ -5,6 +5,6 @@ pip3 install --user --upgrade pip
 pip install --user 'git+https://github.com/cpbotha/xdg-open-wsl.git'
 
 # Dotfiles
-cat ~/dotfiles/templates/wsl/.zshenv >> ~/.zshenv
-cat ~/dotfiles/templates/wsl/.zshrc >> ~/.zshrc
-cat ~/dotfiles/templates/wsl/.zlogin >> ~/.zlogin
+cat ~/dotfiles/templates/wsl/zshenv >> ~/.zshenv
+cat ~/dotfiles/templates/wsl/zshrc >> ~/.zshrc
+cat ~/dotfiles/templates/wsl/zlogin >> ~/.zlogin
