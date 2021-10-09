ec36a8174a09023130d7457264184fb6ef43cbb8
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-09 15:57:33 -0700

    install sift settings

--- shell/settings.sh
+++ shell/settings.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.config
 cp ~/dotfiles/templates/shell/zshenv ~/.zshenv
 cp ~/dotfiles/templates/shell/zshrc ~/.zshrc
 cp ~/dotfiles/templates/shell/zlogin ~/.zlogin
+cp ~/dotfiles/templates/.gitconfig ~/
 cp --recursive ~/dotfiles/micro/. ~/.config/micro/
 cp --recursive ~/dotfiles/starship/. ~/.config/
-cp ~/dotfiles/templates/.gitconfig ~/
+cp --recursive ~/dotfiles/sift/. ~/
