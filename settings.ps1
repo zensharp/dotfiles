1913d2d23a23dd717e9f2d71c3ac1203cf622618
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-10 17:37:08 -0700

    fix windows installer

--- settings.ps1
+++ settings.ps1
@@ -1,6 +1,4 @@
-﻿$dotfiles = "$env:USERPROFILE\dotfiles"
-
-Copy-Item -Recurse -Force ueli $env:APPDATA/ueli
-Copy-Item -Recurse -Force windows-terminal/* $env:LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
-Copy-Item -Recurse -Force vscode $env:APPDATA/Code/User
-Copy-Item -Recurse -Force atom $env:USERPROFILE/.atom
+Copy-Item -Recurse -Force ueli/* $env:APPDATA/ueli/
+Copy-Item -Recurse -Force windows-terminal/* $env:LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+Copy-Item -Recurse -Force vscode/* $env:APPDATA/Code/User/
+Copy-Item -Recurse -Force atom/* $env:USERPROFILE/.atom/
