cec58c489d4150223cc39a3896637988bc7881ea
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-25 20:32:05 -0700

    fix paths

--- templates/linux/settings.sh
+++ templates/linux/settings.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp ~/dotfiles/templates/shell/zshenv ~/.zshenv
-cp ~/dotfiles/templates/shell/zshrc ~/.zshrc
-cp ~/dotfiles/templates/shell/zlogin ~/.zlogin
+cp ~/dotfiles/templates/linux/zshenv ~/.zshenv
+cp ~/dotfiles/templates/linux/zshrc ~/.zshrc
+cp ~/dotfiles/templates/linux/zlogin ~/.zlogin
