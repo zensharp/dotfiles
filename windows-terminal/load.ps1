6f054230d75aaa092b505d02e9093e2aebb24075
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-10-28 11:05:45 -0700

    add wt save/load

--- windows-terminal/load.ps1
+++ windows-terminal/load.ps1
@@ -1,4 +1,4 @@
 $settingsRoot = $MyInvocation.MyCommand.Path
 $settingsRoot = Split-Path $MyInvocation.MyCommand.Path
 
-Copy-Item -Recurse -Force "$env:APPDATA/ueli" "$settingsRoot/config.json"
+Copy-Item -Recurse -Force "$settingsRoot/config.json" "$env:APPDATA/ueli/"
