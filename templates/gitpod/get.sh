f0f9921554d8ef7bc49adf669e7512a2cb0eb501
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-11-12 14:06:34 -0800

    install cascadia automatically

--- templates/gitpod/get.sh
+++ templates/gitpod/get.sh
@@ -3,5 +3,6 @@
 git clone https://gitlab.com/andtech/dotfiles ~/dotfiles
 ~/dotfiles/templates/gitpod/setup.sh
 ~/dotfiles/git/apply.sh
+~/dotfiles/fonts/cascadia/install.sh
 ~/dotfiles/starship/install.sh
 ~/dotfiles/starship/apply.sh
