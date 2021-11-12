8a38285dd396b61a2d2a75cfc3bb33b9b161bbee
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-11-12 12:09:16 -0800

    add shell/zsh installers

---
+++ shell/apply.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat ~/dotfiles/shell/env >> ~/.zshenv
+cat ~/dotfiles/shell/interactive >> ~/.zshrc
+cat ~/dotfiles/shell/login >> ~/.zlogin
---
+++ zsh/apply.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat ~/dotfiles/zsh/env >> ~/.zshenv
+cat ~/dotfiles/zsh/interactive >> ~/.zshrc
+cat ~/dotfiles/zsh/login >> ~/.zlogin
