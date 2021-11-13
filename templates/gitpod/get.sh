b9f52511332a74fb5d8be7c91edbcd32add77bb0
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-11-13 06:53:02 +0000

    Update get.sh

--- templates/gitpod/get.sh
+++ templates/gitpod/get.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://gitlab.com/andtech/dotfiles ~/dotfiles
+git clone https://gitlab.com/andtech/dotfiles.git ~/dotfiles
 ~/dotfiles/templates/gitpod/setup.sh
 ~/dotfiles/git/apply.sh
 ~/dotfiles/fonts/cascadia/install.sh
