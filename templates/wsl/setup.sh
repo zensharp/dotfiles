f6d9048dd0e23214dcc4cc3271795b963e80a39c
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-11-28 12:36:50 -0800

    add xdg user directory

--- templates/wsl/setup.sh
+++ templates/wsl/setup.sh
@@ -5,3 +5,4 @@
 ~/dotfiles/wsl/apply.sh
 echo '#export XDG_REPOS_DIR="/mnt/c/users/andrew/repos"' >> ~/.zshenv
 echo '#export XDG_MUSIC_DIR="/mnt/c/users/andrew/music"' >> ~/.zshenv
+echo '#export XDG_DOWNLOAD_DIR="/mnt/c/users/andrew/downloads"' >> ~/.zshenv
