bf7b4ed178957272f8a9f9babaa9fd79af28da37
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-13 15:58:38 -0800

    remove old settings

--- ueli/apply.ps1
+++
@@ -1,4 +0,0 @@
-$settingsRoot = $MyInvocation.MyCommand.Path
-$settingsRoot = Split-Path $MyInvocation.MyCommand.Path
-
-Copy-Item -Recurse -Force "$settingsRoot/config.json" "$env:APPDATA/ueli/"
--- ueli/save.ps1
+++
@@ -1,4 +0,0 @@
-$settingsRoot = $MyInvocation.MyCommand.Path
-$settingsRoot = Split-Path $MyInvocation.MyCommand.Path
-
-Copy-Item -Recurse -Force "$env:APPDATA/ueli/config.json" "$settingsRoot/"
