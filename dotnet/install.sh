25ddb0e87163e6b183cfc288bde59656da16e52f
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-16 12:34:27 -0800

    update dotnet dotfiles

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-export DOTNET_ROOT=$HOME/.dotnet
-wget -O "$DOTNET_ROOT/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
-chmod +x "$DOTNET_ROOT/dotnet-install.sh"
-"$DOTNET_ROOT/dotnet-install.sh" --channel 6.0
+export DOTNET_ROOT=$HOME/dotnet
+mkdir -p $DOTNET_ROOT
+curl -fsSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir $DOTNET_ROOT
 export $PATH=$PATH:$DOTNET_ROOT
