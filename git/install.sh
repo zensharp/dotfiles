723f5f044e29d68efc7d98b61c0921962e6a8ddc
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-16 00:08:29 -0800

    add git settings installer

---
+++ git/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/bin
+git clone https://github.com/AndrewMJordan/git-tools ~/.local/bin/git-tools
--- templates/gitpod/get.sh
+++ templates/gitpod/get.sh
@@ -2,6 +2,7 @@
 
 git clone https://gitlab.com/andtech/dotfiles.git ~/dotfiles
 ~/dotfiles/templates/gitpod/setup.sh
+~/dotfiles/git/install.sh
 ~/dotfiles/git/apply.sh
 ~/dotfiles/fonts/cascadia/install.sh
 ~/dotfiles/starship/install.sh
