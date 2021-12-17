417df23559250e534f9296e117921eccaf03b42d
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-17 01:23:22 +0000

    Update install.sh

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -3,4 +3,4 @@
 export DOTNET_ROOT=$HOME/dotnet
 mkdir -p $DOTNET_ROOT
 curl -fsSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir $DOTNET_ROOT
-export $PATH=$PATH:$DOTNET_ROOT
+export PATH=$PATH:$DOTNET_ROOT
