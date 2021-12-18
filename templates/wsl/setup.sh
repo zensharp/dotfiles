de6f7f4ae84839e6ff38b95adccee16089b91457
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-17 21:37:54 -0800

    update templates

--- templates/wsl/setup.sh
+++ templates/wsl/setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+~/dotfiles/shell/install.sh
 ~/dotfiles/shell/apply.sh
 ~/dotfiles/zsh/apply.sh
 ~/dotfiles/wsl/apply.sh
+~/dotfiles/git/apply.sh
+~/dotfiles/starship/apply.sh
 echo '#export XDG_REPOS_DIR="/mnt/c/users/andrew/repos"' >> ~/.zshenv
 echo '#export XDG_MUSIC_DIR="/mnt/c/users/andrew/music"' >> ~/.zshenv
 echo '#export XDG_DOWNLOAD_DIR="/mnt/c/users/andrew/downloads"' >> ~/.zshenv
