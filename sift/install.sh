10e3e3715d4a8b23bd5c21692a6a15e62df6f5ef
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-20 04:51:42 -0800

    improve sift cleanup

--- sift/install.sh
+++ sift/install.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/.local/bin
 
 curl https://sift-tool.org/downloads/sift/sift_latest_linux_amd64.tar.gz | tar -xz
-mv sift_*/sift ~/.local/bin/sift
+mv sift_*/sift ~/.local/bin/
+rm -rf sift_*
 
 ~/dotfiles/sift/apply.sh
