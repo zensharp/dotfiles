21ebdf1c69e0365fcf3651c8bda408c976376524
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-19 19:11:09 -0800

    update templates

--- templates/gitpod/setup.sh
+++ templates/gitpod/setup.sh
@@ -5,8 +5,9 @@ sudo apt update
 ~/dotfiles/shell/apply.sh
 ~/dotfiles/zsh/install.sh
 ~/dotfiles/nodejs/install.sh
+#~/dotfiles/dotnet/install.sh
 #~/dotfiles/micro/install.sh
+~/dotfiles/git/install.sh
 ~/dotfiles/sift/install.sh
 ~/dotfiles/misc/install.sh
-~/dotfiles/git/install.sh
 ~/dotfiles/starship/install.sh
--- templates/wsl/setup.sh
+++ templates/wsl/setup.sh
@@ -5,10 +5,11 @@ sudo apt update
 ~/dotfiles/shell/apply.sh
 ~/dotfiles/zsh/install.sh
 ~/dotfiles/nodejs/install.sh
+~/dotfiles/dotnet/install.sh
 ~/dotfiles/micro/install.sh
+~/dotfiles/git/install.sh
 ~/dotfiles/sift/install.sh
 ~/dotfiles/misc/install.sh
-~/dotfiles/git/install.sh
 ~/dotfiles/starship/install.sh
 ~/dotfiles/python/install.sh
 ~/dotfiles/wsl/apply.sh
