9076fd8abee714224c632dc0c4b97209d4cc0c9d
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-26 11:30:37 -0800

    add dotnet extras

---
+++ dotnet/install-extras.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+dotnet nuget add source "https://gitlab.com/api/v4/projects/29250253/packages/nuget/index.json" --name gitlab/andtech
+dotnet tool install -g dotnet-script;
+dotnet tool install -g andtech.gooball;
+dotnet tool install -g andtech.raccoon;
+dotnet tool install -g andtech.snip;
+dotnet tool install -g andtech.jot;
+dotnet tool install -g andtech.dj;
+dotnet tool install -g andtech.to;
+dotnet tool install -g andtech.dpm;
