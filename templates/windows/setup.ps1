bd0205757a4c599acdfa0bb4a2565d106be2f1af
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-26 17:46:02 -0800

    add win64 tools

---
+++ templates/windows/setup.ps1
@@ -0,0 +1,17 @@
+winget install --exact --id Microsoft.dotnet
+winget install --exact --id Microsoft.WindowsTerminal.Preview
+winget install --exact --id Microsoft.PowerShell
+winget install --exact --id Microsoft.VisualStudioCode
+winget install --exact --id Bitwarden.Bitwarden
+winget install --exact --id UnityTechnologies.UnityHub
+winget install --exact --id Mozilla.Firefox
+winget install --exact --id Google.Chrome
+winget install --exact --id Notepads.Notepads
+winget install --exact --id SlackTechnologies.Slack
+winget install --exact --id Discord.Discord
+winget install --exact --id Spotify.Spotify
+winget install --exact --id Nvidia.GeForceExperience
+winget install --exact --id VideoLAN.VLC
+winget install --exact --id Valve.Steam
+winget install --exact --id HermannSchinagl.LinkShellExtension
+#winget install --exact --id Blizzard.BattleNet
