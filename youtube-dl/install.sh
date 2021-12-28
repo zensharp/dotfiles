a5b616dc56a2dfab8bc0c565ce6bbd5c61161e5a
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-27 23:26:55 -0800

    update youtube-dl installer

--- youtube-dl/install.sh
+++ youtube-dl/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt update
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo apt install -y ffmpeg
