345dce71dcfdae3327c7330a3b536e95c25727d9
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-27 23:25:44 -0800

    update apt-fast

--- apt-fast/apply.sh
+++ apt-fast/apply.sh
@@ -3,3 +3,6 @@
 echo '. ~/dotfiles/apt-fast/env' >> ~/.zshenv
 echo '. ~/dotfiles/apt-fast/interactive' >> ~/.zshrc
 echo '. ~/dotfiles/apt-fast/login' >> ~/.zlogin
+. ~/dotfiles/apt-fast/env
+. ~/dotfiles/apt-fast/interactive
+. ~/dotfiles/apt-fast/login
