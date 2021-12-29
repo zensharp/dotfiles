c352109d65a875840723706acec01a99960deb2f
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-29 12:45:40 -0800

    fix go dotfiles

--- go/apply.sh
+++ go/apply.sh
@@ -2,7 +2,7 @@
 
 echo '. ~/dotfiles/go/env' >> ~/.zshenv
 echo '. ~/dotfiles/go/interactive' >> ~/.zshrc
-echo '. ~/dotfiles/go/login' >> ~/.zlogin. ~/dotfiles/go/env
+echo '. ~/dotfiles/go/login' >> ~/.zlogin.
 . ~/dotfiles/go/env
 . ~/dotfiles/go/interactive
 . ~/dotfiles/go/login
