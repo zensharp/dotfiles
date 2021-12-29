300fc3dd886e78dc16d2b4330d7e07e67751f744
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-28 17:53:13 -0800

    fix apply script

--- go/apply.sh
+++ go/apply.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cat ~/dotfiles/go/env > ~/.zshenv
-cat ~/dotfiles/go/interactive > ~/.zshrc
-cat ~/dotfiles/go/login > ~/.zlogin
+cat ~/dotfiles/go/env >> ~/.zshenv
+cat ~/dotfiles/go/interactive >> ~/.zshrc
+cat ~/dotfiles/go/login >> ~/.zlogin
 . ~/dotfiles/go/env
 . ~/dotfiles/go/interactive
 . ~/dotfiles/go/login
