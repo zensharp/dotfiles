352e8127400a21034b7e181c0efbc103a66dc28d
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-29 21:52:34 -0800

    add live dotfiles reading

--- dotnet/load.sh
+++ dotnet/load.sh
@@ -3,3 +3,6 @@
 echo '. ~/dotfiles/dotnet/env' >> ~/.zshenv
 echo '. ~/dotfiles/dotnet/interactive' >> ~/.zshrc
 echo '. ~/dotfiles/dotnet/login' >> ~/.zlogin
+. ~/dotfiles/dotnet/env
+. ~/dotfiles/dotnet/interactive
+. ~/dotfiles/dotnet/login
