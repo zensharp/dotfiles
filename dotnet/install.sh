9b54da4da6e808da0f6db769b9ab2a02b0103dfc
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-29 23:46:02 -0800

    fix dotnet dotfiles

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -2,5 +2,5 @@
 
 export DOTNET_ROOT=$HOME/dotnet
 mkdir -p $DOTNET_ROOT
-curl -fsSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir $DOTNET_ROOT
-export PATH=$PATH:$DOTNET_ROOT
+curl -fsSL "https://dot.net/v1/dotnet-install.sh" | bash /dev/stdin --channel Current --install-dir $DOTNET_ROOT
+. ~/dotfiles/dotnet/apply.sh
