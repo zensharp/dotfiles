6e0ba0437035971592299458314c44c569fef199
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-30 14:11:56 -0800

    revert wsl setp
    Implementing feature "fast"

--- templates/wsl/setup.sh
+++ templates/wsl/setup.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 . ~/dotfiles/apt-fast/install.sh
-which apt
-which apt-get
 . ~/dotfiles/shell/load.sh
 . ~/dotfiles/zsh/install.sh
-#. ~/dotfiles/node/install.sh
+. ~/dotfiles/node/install.sh
 . ~/dotfiles/dotnet/install.sh
-#. ~/dotfiles/micro/install.sh
+. ~/dotfiles/micro/install.sh
 . ~/dotfiles/git/install.sh
-#. ~/dotfiles/bat/install.sh
-#. ~/dotfiles/delta/install.sh
-#. ~/dotfiles/fd-find/install.sh
-#. ~/dotfiles/sd/install.sh
-#. ~/dotfiles/sift/install.sh
+. ~/dotfiles/bat/install.sh
+. ~/dotfiles/delta/install.sh
+. ~/dotfiles/fd-find/install.sh
+. ~/dotfiles/sd/install.sh
+. ~/dotfiles/sift/install.sh
 . ~/dotfiles/starship/install.sh
-#. ~/dotfiles/misc/install.sh
-#. ~/dotfiles/python/install.sh
+. ~/dotfiles/misc/install.sh
+. ~/dotfiles/python/install.sh
 . ~/dotfiles/wsl/install.sh
 echo '#export XDG_REPOS_DIR="/mnt/c/users/andrew/repos"' >> ~/.zshenv
 echo '#export XDG_MUSIC_DIR="/mnt/c/users/andrew/music"' >> ~/.zshenv
