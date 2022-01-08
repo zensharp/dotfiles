fe8bd684b458cb0d8f1f3460fd713f084253412f
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-01-07 16:41:57 -0800

    add dpm installer

---
+++ templates/wsl/dpm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+dpm install -s micro
+dpm install -s starship
+dpm install -s visual-studio-code --wsl
+dpm install -s windows-terminal --wsl
+dpm install vlc --wsl
+dpm install doom --wsl
