832fa1821da32bd900462d486481a263bdc28280
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-01-29 07:51:08 -0800

    update windows templates

--- templates/windows/setup.ps1
+++ templates/windows/setup.ps1
@@ -1,17 +1,24 @@
-winget install --exact --id Microsoft.dotnet
+# Essentials
 winget install --exact --id Microsoft.WindowsTerminal.Preview
-winget install --exact --id Microsoft.PowerShell
-winget install --exact --id Microsoft.VisualStudioCode
+winget install --exact --id HermannSchinagl.LinkShellExtension
+winget install --exact --id VideoLAN.VLC
 winget install --exact --id Bitwarden.Bitwarden
-winget install --exact --id UnityTechnologies.UnityHub
 winget install --exact --id Mozilla.Firefox
 winget install --exact --id Google.Chrome
 winget install --exact --id Notepads.Notepads
-winget install --exact --id SlackTechnologies.Slack
 winget install --exact --id Discord.Discord
+winget install --exact --id SlackTechnologies.Slack
+
+# Development
+winget install --exact --id Microsoft.dotnet
+winget install --exact --id Microsoft.PowerShell
+winget install --exact --id Microsoft.VisualStudio.2022.Community
+winget install --exact --id Microsoft.VisualStudioCode
+winget install --exact --id UnityTechnologies.UnityHub
+
+# Entertainment
 winget install --exact --id Spotify.Spotify
-winget install --exact --id Nvidia.GeForceExperience
-winget install --exact --id VideoLAN.VLC
+winget install --exact --id ImageLine.FLStudio
 winget install --exact --id Valve.Steam
-winget install --exact --id HermannSchinagl.LinkShellExtension
-#winget install --exact --id Blizzard.BattleNet
+winget install --exact --id Blizzard.BattleNet
+winget install --exact --id Nvidia.GeForceExperience
