5a0b2a3f2a5145dccdececad27580ffc7f12b8a8
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-01-29 20:22:23 -0800

    update hugo/install.sh

--- hugo/install.sh
+++ hugo/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget "https://github.com/gohugoio/hugo/releases/download/v0.90.1/hugo_extended_0.90.1_Linux-64bit.deb" \
+wget "https://github.com/gohugoio/hugo/releases/download/v0.92.1/hugo_extended_0.92.1_Linux-64bit.deb" \
     -O hugo.deb; \
     sudo dpkg -i hugo.deb; \
     rm hugo.deb
