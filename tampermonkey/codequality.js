86037fd1b602aa3cba4636b498ff732b7fb9baec
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-02-21 12:08:39 -0800

    update "codequality.js"

--- tampermonkey/codequality.js
+++ tampermonkey/codequality.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Code Quality Widget Fix
 // @namespace    Andtech
-// @version      0.3.2
+// @version      0.3.3
 // @match        https://gitlab.com/*/-/merge_requests/*
 // @updateurl    https://gitlab.com/andtech/dotfiles/-/raw/master/tampermonkey/codequality.js
 // @downloadurl  https://gitlab.com/andtech/dotfiles/-/raw/master/tampermonkey/codequality.js
@@ -11,25 +11,30 @@
 
 (function() {
     'use strict';
-    console.log("Tamper Monkey Script loaded");
-    /* get code quality report for this issue */
-    var cqurl = window.location.href + "/codequality_reports.json";
-    const response = fetch(cqurl);
-    const report = response.json();
-    console.log("Code Quality json loaded");
 
-    var new_err = report.new_errors.length;
-    var fixed_err = report.resolved_errors.length;
-    var delta = new_err-fixed_err;
-
-    /* add plus sign for positive change */
-    if ((delta) > 0) {
-        delta = "+" + delta;
-    }
-
-    /* add the info to the already-displayed text */
-    $("div.js-code-text")[0].innerText += "\nResolved: " + fixed_err + "  New: " + new_err;
-    $("div.js-code-text")[0].innerText += "\nChange: " + (delta);
+    async function getCodeQuality() {
+        console.log("Tamper Monkey Script loaded");
+        /* get code quality report for this issue */
+        var cqurl = window.location.href + "/codequality_reports.json";
+        const response = await fetch(cqurl);
+        const report = await response.json();
+        console.log("Code Quality json loaded");
     
-    console.log("Tamper Monkey completed");
+        var new_err = report.new_errors.length;
+        var fixed_err = report.resolved_errors.length;
+        var delta = new_err-fixed_err;
+    
+        /* add plus sign for positive change */
+        if ((delta) > 0) {
+            delta = "+" + delta;
+        }
+    
+        /* add the info to the already-displayed text */
+        $("div.js-code-text")[0].innerText += "\nResolved: " + fixed_err + "  New: " + new_err;
+        $("div.js-code-text")[0].innerText += "\nChange: " + (delta);
+        
+        console.log("Tamper Monkey completed");
+    }
+    
+    getCodeQuality();
 })();
