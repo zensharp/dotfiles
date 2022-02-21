dc6633a50afc614f843fca331f43447a3a9b59da
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-02-21 12:03:22 -0800

    add tampermonkey

---
+++ tampermonkey/codequality.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Code Quality Widget Fix
+// @namespace    Andtech
+// @version      0.3.1
+// @match        https://gitlab.com/*/-/merge_requests/*
+// @updateurl    https://gitlab.com/andtech/dotfiles/-/raw/master/tampermonkey/codequality.js
+// @downloadurl  https://gitlab.com/andtech/dotfiles/-/raw/master/tampermonkey/codequality.js
+// @description  See https://gitlab.com/gitlab-org/gitlab/-/issues/244338#note_720313480
+// @author       andtech
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Tamper Monkey Script loaded");
+    /* get code quality report for this issue */
+    var cqurl = window.location.href + "/codequality_reports.json";
+    const response = await fetch(cqurl);
+    const report = await response.json();
+    console.log("Code Quality json loaded");
+
+    var new_err = report.new_errors.length;
+    var fixed_err = report.resolved_errors.length;
+    var delta = new_err-fixed_err;
+
+    /* add plus sign for positive change */
+    if ((delta) > 0) {
+        delta = "+" + delta;
+    }
+
+    /* add the info to the already-displayed text */
+    $("div.js-code-text")[0].innerText += "\nResolved: " + fixed_err + "  New: " + new_err;
+    $("div.js-code-text")[0].innerText += "\nChange: " + (delta);
+    
+    console.log("Tamper Monkey completed");
+})();
