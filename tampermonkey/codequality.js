3d4b8673d6196f4142f8e0d8968ad5876965a148
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-02-21 12:42:53 -0800

    update "codequality.js"

--- tampermonkey/codequality.js
+++ tampermonkey/codequality.js
@@ -2,7 +2,7 @@
 // @name         Code Quality Widget Fix
 // @namespace    Andtech
 // @author       andtech
-// @version      0.3.11
+// @version      0.3.12
 // @match        https://gitlab.com/*/-/merge_requests/*
 // @run-at       document-end
 // @updateurl    https://gitlab.com/andtech/dotfiles/-/raw/master/tampermonkey/codequality.js
@@ -36,7 +36,7 @@
         var fixed_err = report.resolved_errors.length;
 
         /* add the info to the already-displayed text */
-        el.innerText += "\nImproved: " + fixed_err + "  Degraded: " + new_err;
+        el.innerText += "(Fixed: " + fixed_err + "  New: " + new_err + ")";
 
         console.log("Tamper Monkey completed");
     }
