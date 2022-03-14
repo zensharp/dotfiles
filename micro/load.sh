813bbf3891a8153ddd20857855ea171f881f2bc6
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-03-14 00:48:55 -0700

    fix micro installer

--- micro/load.sh
+++ micro/load.sh
@@ -3,3 +3,5 @@
 mkdir -p ~/.config/micro
 cp ~/.dotfiles/micro/settings.json ~/.config/micro/
 cp ~/.dotfiles/micro/bindings.json ~/.config/micro/
+cp -r ~/.dotfiles/micro/syntax/ ~/.config/micro/
+cp -r ~/.dotfiles/micro/colorschemes/ ~/.config/micro/
