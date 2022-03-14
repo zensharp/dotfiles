95dc25366c61d5c7fbf66959c77a8930187830e4
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-03-14 01:12:34 -0700

    improve exa installation storage

--- exa/install.sh
+++ exa/install.sh
@@ -4,10 +4,11 @@ if command -v brew &> /dev/null
 then
 	brew install exa
 else
-	mkdir exa
-	cd exa
+	mkdir /tmp/exa
+	cd /tmp/exa
 	wget -O temp.zip 'https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip'
 	unzip temp.zip
 	mkdir -p $HOME/.local/bin
 	mv bin/* $HOME/.local/bin
+	cd -
 fi
