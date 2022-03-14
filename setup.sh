28b34ac197f8d821c75e0006eac8ae49b273feae
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-03-13 23:24:19 -0700

    update installer

--- setup.sh
+++ setup.sh
@@ -23,10 +23,17 @@ case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
 		;;
 esac
 
-if [[ "$platform" == "unknown" ]]; then
+if [[ "$platform" == "unknown" ]]
+then
 	echo "[ERROR] Unsupported platform: $platform"
 	exit 1
-fi	
+fi
+
+if $DOTFILES_SKIP
+then
+	echo -e "\033[0;35mSkipping dotfiles!\033[0m"
+	exit 0
+fi
 
 echo -e "\033[0;35mLite mode enabled!\033[0m"
 echo -e "\033[0;36mSetting up dotfiles for platform '$platform'...\033[0m"
