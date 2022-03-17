594c8b20904c1ea021880aa13b16953b51ba782d
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-03-16 17:16:48 -0700

    delete generic load template

--- templates/load.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dpm install micro linux
-dpm install sift linux
-dpm install starship linux
-dpm install windows-terminal wsl
-dpm install visual-studio-code wsl
-dpm install vlc wsl
