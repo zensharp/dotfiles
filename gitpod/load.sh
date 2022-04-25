02837847be55b9d41f92d9b369814ac3a622b50d
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-04-25 16:09:48 -0700

    improve open

---
+++ gitpod/load.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo '. ~/.dotfiles/gitpod/env' >> ~/.zshenv
+echo '. ~/.dotfiles/gitpod/interactive' >> ~/.zshrc
+echo '. ~/.dotfiles/gitpod/login' >> ~/.zlogin
---
+++ gitpod/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+. ~/.dotfiles/gitpod/install.sh
+. ~/.dotfiles/gitpod/load.sh
