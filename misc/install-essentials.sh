14b357a2aaf10935430fb568e195d927412ec3bd
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-05-13 13:15:17 -0700

    install zoxide

--- misc/install-essentials.sh
+++ misc/install-essentials.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get -qq update
 sudo apt-get -qq install -y neofetch
-sudo apt-get -qq install -y autojump
 sudo npm install --global undollar
 sudo npm install --global clipboard-cli
 sudo npm install --global live-server
+
+curl -sS https://webinstall.dev/zoxide | bash
