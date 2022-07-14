3f5e059f6f735b933346d0ecadc5c52b95ed04aa
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-07-14 02:48:04 -0700

    update node installer

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get -qq install -y nodejs
