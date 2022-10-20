92ac82c0fc6b23f18a00951f83a79c8935a5370f
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-10-20 11:15:07 -0700

    remove old packages

--- dotnet/install-extras.sh
+++ dotnet/install-extras.sh
@@ -4,7 +4,5 @@ dotnet tool install -g dotnet-script;
 dotnet tool install -g andtech.gooball;
 dotnet tool install -g andtech.raccoon;
 dotnet tool install -g andtech.snip;
-dotnet tool install -g andtech.jot;
 dotnet tool install -g andtech.dj;
 dotnet tool install -g andtech.to;
-dotnet tool install -g andtech.dpm;
