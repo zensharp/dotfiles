66f8709cd4047c5a64ed7ceb1fc12e876ded6175
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-12-13 21:22:20 -0800

    improve dotfiles

--- wsl/setup.sh
+++ wsl/setup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# WSL Utilities
+sudo apt install gnupg2 apt-transport-https
+wget -O - https://pkg.wslutiliti.es/public.key | sudo tee -a /etc/apt/trusted.gpg.d/wslu.asc
+echo "deb https://pkg.wslutiliti.es/debian bullseye main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install wslu
+
+# Misc
 pip3 install --user --upgrade pip
 pip install --user 'git+https://github.com/cpbotha/xdg-open-wsl.git'
