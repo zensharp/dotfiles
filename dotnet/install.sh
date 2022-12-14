21cf07641441c97db2778f22e85fd3f7861cd31c
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-12-13 20:44:18 -0800

    fix dotnet installation

--- dotnet/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export DOTNET_ROOT=$HOME/dotnet
-mkdir -p $DOTNET_ROOT
-curl -fsSL "https://dot.net/v1/dotnet-install.sh" | bash /dev/stdin --channel Current --install-dir $DOTNET_ROOT
--- dotnet/setup.sh
+++ dotnet/setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-. ~/.dotfiles/dotnet/install.sh
+# Install SDK
+wget https://packages.microsoft.com/config/ubuntu/22.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-7.0
+sudo apt-get install -y dotnet-sdk-6.0
+
+# Misc
 . ~/.dotfiles/dotnet/load.sh
 
 dotnet nuget add source "https://gitlab.com/api/v4/projects/29250253/packages/nuget/index.json" --name gitlab/andtech
