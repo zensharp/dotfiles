e373028113f932a954cbe63bf1888290fe78da40
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-12-15 22:41:59 -0800

    rename site folders

--- gather.sh
+++ gather.sh
@@ -3,6 +3,6 @@
 for FILE in $(find . -name README.md -not -path ./README.md -not -path "./site/*")
 do
 	NAME="$(basename "$(dirname "$FILE")")"
-	mkdir -p "site/misc"
-	cp "$FILE" "site/misc/$NAME.md"
+	mkdir -p "site/apps"
+	cp "$FILE" "site/apps/$NAME.md"
 done
