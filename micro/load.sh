cdb60edcc8ffd09634f128616feec53bd979612e
Zensharp <48300131+zensharp@users.noreply.github.com>
2023-08-02 11:44:41 -0700

    update "iosevka-custom-semibold.ttf", "load.sh"

--- micro/load.sh
+++ micro/load.sh
@@ -3,5 +3,6 @@
 mkdir -p ~/.config/micro
 cp ~/.dotfiles/micro/settings.json ~/.config/micro/
 cp ~/.dotfiles/micro/bindings.json ~/.config/micro/
+
 cp -r ~/.dotfiles/micro/syntax/ ~/.config/micro/
 cp -r ~/.dotfiles/micro/colorschemes/ ~/.config/micro/
