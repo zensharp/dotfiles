ad233cfc8a3daf3768c1677614874a7caebad837
Andrew <andrewmjordan@users.noreply.github.com>
2025-02-17 11:13:46 +0000

    add micro

--- templates/gitpod/setup.sh
+++ templates/gitpod/setup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# Setup profiles
+echo ". $HOME/dotfiles/templates/gitpod/zshrc" >> $HOME/.zshrc
+echo ". $HOME/dotfiles/templates/gitpod/zshenv" >> $HOME/.zshenv
+
 # Install starship
 curl https://get.zensharp.net/starship | bash
 ~/dotfiles/starship/load.sh
 
 # Install zoxide
 curl https://get.zensharp.net/zoxide | bash
+
+# Install micro
+brew install micro
+~/dotfiles/micro/load.sh
 
 # Install eza
 sudo apt update
@@ -16,6 +24,3 @@ echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
 sudo apt install -y eza
-
-echo ". $HOME/dotfiles/templates/gitpod/zshrc" >> $HOME/.zshrc
-echo ". $HOME/dotfiles/templates/gitpod/zshenv" >> $HOME/.zshenv
