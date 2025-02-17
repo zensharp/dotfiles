b4f014da7669df44e050e35edd3260c9ebe8356f
Andrew <andrewmjordan@users.noreply.github.com>
2025-02-17 11:07:19 +0000

    refactor gitpod

--- templates/gitpod/setup.sh
+++ templates/gitpod/setup.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-#. ~/dotfiles/templates/core/setup.sh
+# Install starship
+curl https://get.zensharp.net/starship | bash
+~/dotfiles/starship/load.sh
+
+# Install zoxide
+curl https://get.zensharp.net/zoxide | bash
+
+# Install eza
+sudo apt update
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
+echo ". $HOME/dotfiles/templates/gitpod/zshrc" >> $HOME/.zshrc
+echo ". $HOME/dotfiles/templates/gitpod/zshenv" >> $HOME/.zshenv
