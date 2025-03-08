a2e9fca4f3df02ee7e3450d5e1a6514e910d34e3
Andrew <andrewmjordan@users.noreply.github.com>
2025-03-08 06:48:00 +0000

    update git integration

--- templates/gitpod/setup.sh
+++ templates/gitpod/setup.sh
@@ -27,3 +27,6 @@ echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
 sudo apt install -y eza
+
+# Install delta
+brew install git-delta
